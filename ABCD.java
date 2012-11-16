cd9f4c88c2b7d7f7501dacfab31e9c22e2547c59
Myles <osyrus@me.com>
2012-11-17 00:06:00 +1030

    Revert "Started to make the ABCD class do 2D"
    This reverts commit c9614603f855e886190020c0ab402e712fbced21.

--- ABCD.java
+++ ABCD.java
@@ -1,98 +1,43 @@
 
 public class ABCD {
-    private int A1;
-    private int B1;
-    private int C1;
-    private int D1;
-    private int A2;
-    private int B2;
-    private int C2;
-    private int D2;
+    private int A;
+    private int B;
+    private int C;
+    private int D;
 
-    //Constructor, sets A, B, C and D for both sides explicitly
-    public ABCD(int A1, int B1, int C1, int D1, int A2, int B2, int C2, int D2) {
-        this.A1 = A1;
-        this.B1 = B1;
-        this.C1 = C1;
-        this.D1 = D1;
-        this.A2 = A2;
-        this.B2 = B2;
-        this.C2 = C2;
-        this.D2 = D2;
-    }
-    //Constructor for only one direction set
-    public ABCD(int A, int B, int C, int D) {
-        super(A, B, C, D, A, B, C, D)
-    }
-    //Constructor for no elements given
-    public ABCD() {
-        super(0, 0, 0, 0)
+    //Constructor, sets A, B, C and D.
+    //Note: change this so that the default (with not A,B,C,D set) is zero.
+    public ABCD(int A,int B,int C,int D) {
+        this.A = A;
+        this.B = B;
+        this.C = C;
+        this.D = D;
     }
 
-    //Returns the ABCD matrix in a two dimensional matrix form dependant on which direction was asked for
+    //Returns the ABCD matrix in a two dimensional matrix form.
     //Note: Figure out the ordering of the array for later mat manips.
-    public int[][] getMat(int mat) {
-        private int[][] ABCDMat;
-
-        if mat == 1
-            ABCDMat = new int[][] {{A1, B1},{C1, D1}};
-        else //should this be else if?
-            ABCDMat = new int[][] {{A2, B2},{C2, D2}};
-
+    public int[][] getMat() {
+        private int[][] ABCDMat = new int[][] {{A, B},{C, D}};
         return ABCDMat;
     }
 
-    //Sets the values of A,B,C and D from a matrix with choice to direction
-    //Note: Overload for default of A1?
-    public void setMat(int[][] ABCDMat, int mat) {
-        if mat == 1 {
-            this.A1 = ABCDMat[1][1];
-            this.B1 = ABCDMat[1][2];
-            this.C1 = ABCDMat[2][1];
-            this.D1 = ABCDMat[2][2];
-        } else {
-            this.A2 = ABCDMat[1][1];
-            this.B2 = ABCDMat[1][2];
-            this.C2 = ABCDMat[2][1];
-            this.D2 = ABCDMat[2][2];
-        }
-        
-    }
-
-    //Returns the requested matrix elements of a specified direction
-    //Note: Overload?
-    public int getA(int mat) {
-        if mat == 1
-            return A1;
-        else
-            return A2;
+    //Returns the requested matrix elements
+    public int getA() {
+        return A;
     }
     public int getB() {
-        if mat == 1
-            return B1;
-        else
-            return B2;
+        return B;
     }
     public int getC() {
-        if mat == 1
-            return C1;
-        else
-            return C2;
+        return C;
     }
     public int getD() {
-        if mat == 1
-            return D1;
-        else
-            return D2;
+        return D;
     }
 
-    //Sets the requested matrix elements for specified direction
-    //Note: Overload?
-    public void setA(int A, int mat) {
-        if mat == 1
-            this.A1 = A;
-        else
-            this.A2 = A;
+    //Sets the requested matrix elements
+    public void setA(int A) {
+        this.A = A;
     }
     public void setB(int B) {
         this.B = B;
--- ThinLens.java
+++
@@ -1,7 +0,0 @@
-
-public class ThinLens extends ABCD {
-    private int f1;
-    private int f2;
-
-    
-}

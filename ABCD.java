86d419f4d8d972b95f239a104e5fdccb6e361e86
Myles <osyrus@me.com>
2012-11-16 17:56:23 +1030

    Flesh Out of ABCD class
    Just added in the main required functions and fixed some errors.

--- ABCD.java
+++ ABCD.java
@@ -1,19 +1,65 @@
 
-class ABCD {
+public class ABCD {
     private int A;
     private int B;
     private int C;
     private int D;
 
-    public ABCD(A,B,C,D) {
+    //Constructor, sets A, B, C and D.
+    //Note: change this so that the default (with not A,B,C,D set) is zero.
+    public ABCD(int A,int B,int C,int D) {
         this.A = A;
         this.B = B;
         this.C = C;
         this.D = D;
     }
 
-    public int[][] returnMat() {
+    //Returns the ABCD matrix in a two dimensional matrix form.
+    //Note: Figure out the ordering of the array for later mat manips.
+    public int[][] getMat() {
         private int[][] ABCDMat = new int[][] {{A, B},{C, D}};
-        return ABCDMat
+        return ABCDMat;
+    }
+
+    //Returns the requested matrix elements
+    public int getA() {
+        return A;
+    }
+    public int getB() {
+        return B;
+    }
+    public int getC() {
+        return C;
+    }
+    public int getD() {
+        return D;
+    }
+
+    //Sets the requested matrix elements
+    public void setA(int A) {
+        this.A = A;
+    }
+    public void setB(int B) {
+        this.B = B;
+    }
+    public void setC(int C) {
+        this.C = C;
+    }
+    public void setD(int D) {
+        this.D = D;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+

7954e4768e572ef7763e14f8bafba5ed4367ebd7
Myles <osyrus@me.com>
2012-11-16 17:44:57 +1030

    Initial Commit
    Wrote the basics behind the generic ABCD matrix class. Not sure if it
    works at all...

---
+++ ABCD.java
@@ -0,0 +1,19 @@
+
+class ABCD {
+    private int A;
+    private int B;
+    private int C;
+    private int D;
+
+    public ABCD(A,B,C,D) {
+        this.A = A;
+        this.B = B;
+        this.C = C;
+        this.D = D;
+    }
+
+    public int[][] returnMat() {
+        private int[][] ABCDMat = new int[][] {{A, B},{C, D}};
+        return ABCDMat
+    }
+}
